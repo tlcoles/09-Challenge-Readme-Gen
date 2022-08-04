c6145f6d6071f7bd0aa9c022ffbff204c0d5cdf9
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-08-04 18:03:53 +0200

    Create sample readme with hyperlinks

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# 09-Challenge-Readme-Gen
+# Challenge 9: README Generator
+
+## Description
 Generate a README file using a command-line application
+
+## Table of Contents
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributing](#contributing)
+[Questions](#questions)
+
+## Installation
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
---
+++ index.js
@@ -0,0 +1,31 @@
+// Require inquirer as a dependency
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+// Create variables
+const HASH = "\u0023"
+const HASH2 = "\u0023\u0023"
+let title = "";
+
+/**
+ *  Create a sample list of template literals
+ *   
+ *  `$(HASH) ${title}`
+ *  `$(HASH2) ${installation}`
+ *  `$(HASH2) ${usage}`
+ *  `$(HASH2) ${license}`
+ *  `$(HASH2) ${contributing}`
+ *  `$(HASH2) ${tests}`
+ *  `$(HASH2) ${questions}`
+**/
